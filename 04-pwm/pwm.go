862035522d1bb18c51fef06e6908af531f4eb08c
Andreas Atle <atle.andreas@gmail.com>
2021-01-13 11:17:00 -0600

    Remove code-duplication (DRY).

--- 04-pwm/pwm.go
+++ 04-pwm/pwm.go
@@ -8,8 +8,8 @@ import (
 // PWM seems to use range 0-65535 rather than 0-255
 const (
 	unitTime = 200
-	high     = 1<<16 - 1
-	low      = 0
+	high     = uint16(1<<16 - 1)
+	low      = uint16(0)
 )
 
 // delay sleeps for n milliseconds.
@@ -18,19 +18,28 @@ func delay(n int) {
 }
 
 // Specify what LED to blink. LED is short for D13.
-var bit1, bit2, bit4, bit8 machine.PWM
+var bit [4]machine.PWM
 
 // init sets up the global environment (Pulse Wave Modulator)
 func init() {
 	machine.InitPWM()
-	bit1 = machine.PWM{Pin: machine.D6}
-	bit2 = machine.PWM{Pin: machine.D9}
-	bit4 = machine.PWM{Pin: machine.D10}
-	bit8 = machine.PWM{Pin: machine.D11}
-	bit1.Configure()
-	bit2.Configure()
-	bit4.Configure()
-	bit8.Configure()
+	bit[0] = machine.PWM{Pin: machine.D6}
+	bit[1] = machine.PWM{Pin: machine.D9}
+	bit[2] = machine.PWM{Pin: machine.D10}
+	bit[3] = machine.PWM{Pin: machine.D11}
+	bit[0].Configure()
+	bit[1].Configure()
+	bit[2].Configure()
+	bit[3].Configure()
+}
+
+func set(i, pwm uint16) {
+	for j := 0; j < 4; j++ {
+		mask := uint16(1 << j)
+		if i&mask == mask {
+			bit[j].Set(pwm)
+		}
+	}
 }
 
 func main() {
@@ -38,33 +47,10 @@ func main() {
 	for {
 		for q := range factors {
 			_ = q
-			for i := 0; i < 16; i++ {
-				high := uint16(1<<i - 1)
-				if i&1 == 1 {
-					bit1.Set(high)
-				}
-				if i&2 == 2 {
-					bit2.Set(high)
-				}
-				if i&4 == 4 {
-					bit4.Set(high)
-				}
-				if i&8 == 8 {
-					bit8.Set(high)
-				}
+			for i := uint16(0); i < 16; i++ {
+				set(i, uint16(1<<i-1))
 				delay(unitTime)
-				if i&1 == 1 {
-					bit1.Set(low)
-				}
-				if i&2 == 2 {
-					bit2.Set(low)
-				}
-				if i&4 == 4 {
-					bit4.Set(low)
-				}
-				if i&8 == 8 {
-					bit8.Set(low)
-				}
+				set(i, low)
 			}
 		}
 	}
