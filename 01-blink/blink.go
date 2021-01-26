dcd6c1c21959a9adf870aa6529182a206a74abe9
Andreas Atle <atle.andreas@gmail.com>
2021-01-26 16:31:18 -0600

    Refactored blink-program.

--- 01-blink/blink.go
+++ 01-blink/blink.go
@@ -1,29 +1,59 @@
 package main
 
 import (
-	"machine"
 	"time"
+
+	"github.com/tinygo-org/tinygo/src/machine"
 )
 
-var intervals = []int{100, 200, 300, 400, 300, 200}
+const (
+	blinkPin = machine.D13
+)
 
-func main() {
-	unit := time.Millisecond
-	led := machine.LED
-	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+// Blink contains info about a blinking LED.
+type Blink struct {
+	pin   machine.Pin
+	delay int
+}
+
+// NewBlink return a new instance of a blinking LED.
+func NewBlink(pin machine.Pin, delay int) *Blink {
+	blink := &Blink{pin: pin, delay: delay}
+	blink.pin.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	return blink
+}
+
+// RunBlink blinks a LED with the current delay.
+func (b *Blink) RunBlink() {
 	for {
-		for _, interval := range intervals {
-			led.High()
-			delay(interval, unit)
-			led.Low()
-			delay(500-interval, unit)
+		b.pin.High()
+		delay(b.delay, time.Millisecond)
+		b.pin.Low()
+		delay(b.delay, time.Millisecond)
+	}
+}
+
+// ControlBlink varies the delay (frequency) of the blinking LED.
+func (b *Blink) ControlBlink() {
+	for {
+		for d := 500; d < 2000; d += 100 {
+			b.delay = d
+			delay(2*d, time.Millisecond)
 		}
 	}
 }
 
+// Main routine for the blinking LED.
+func main() {
+	blink := NewBlink(blinkPin, 1000)
+	go blink.RunBlink()
+	blink.ControlBlink()
+}
+
+// delay is short for time.Sleep
 func delay(length int, unit time.Duration) {
-	time.Sleep(time.Duration(interval) * unit)
+	time.Sleep(time.Duration(length) * unit)
 }
 
 // To get the Arduino to blink:
-// tinygo flash -target arduino blink.go
+// tinygo flash -target arduino -scheduler tasks blink.go
